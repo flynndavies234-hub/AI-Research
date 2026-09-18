34ff83c290b1fe3b96f3cb6b32773ca30a440ebb
flynndavies234-hub <flynndavies234@gmail.com>
2026-09-18 20:09:57 +0200

    Expose V2.5 planner service

--- preload.js
+++ preload.js
@@ -2,6 +2,6 @@ const {contextBridge,ipcRenderer}=require('electron');
 contextBridge.exposeInMainWorld('desktopAPI',{
  getAppInfo:()=>ipcRenderer.invoke('app-info'), saveSettings:x=>ipcRenderer.invoke('save-settings',x),
  checkUpdates:()=>ipcRenderer.invoke('check-updates'),installUpdate:()=>ipcRenderer.invoke('install-update'),
- schoolAsk:x=>ipcRenderer.invoke('school-ask',x), researchAsk:x=>ipcRenderer.invoke('research-ask',x),
+ schoolAsk:x=>ipcRenderer.invoke('school-ask',x), researchAsk:x=>ipcRenderer.invoke('research-ask',x), planAsk:x=>ipcRenderer.invoke('plan-ask',x),
  onUpdateStatus:fn=>ipcRenderer.on('update-status',(_,x)=>fn(x))
 });
