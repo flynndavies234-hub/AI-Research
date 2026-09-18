0c6e6b92080a620095c5b39dc389fed5404defa5
flynndavies234-hub <flynndavies234@gmail.com>
2026-09-18 19:49:40 +0200

    Add preload.js for v2.2.2

---
+++ preload.js
@@ -0,0 +1,7 @@
+const {contextBridge,ipcRenderer}=require('electron');
+contextBridge.exposeInMainWorld('desktopAPI',{
+ getAppInfo:()=>ipcRenderer.invoke('app-info'), saveSettings:x=>ipcRenderer.invoke('save-settings',x),
+ checkUpdates:()=>ipcRenderer.invoke('check-updates'),installUpdate:()=>ipcRenderer.invoke('install-update'),
+ schoolAsk:x=>ipcRenderer.invoke('school-ask',x), researchAsk:x=>ipcRenderer.invoke('research-ask',x),
+ onUpdateStatus:fn=>ipcRenderer.on('update-status',(_,x)=>fn(x))
+});
