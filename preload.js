e3bc7ef2e5dc59f0cea6711b559c7e10d148294e
flynndavies234-hub <flynndavies234@gmail.com>
2026-09-18 20:11:05 +0200

    Expose diagnostics controls

--- preload.js
+++ preload.js
@@ -3,5 +3,5 @@ contextBridge.exposeInMainWorld('desktopAPI',{
  getAppInfo:()=>ipcRenderer.invoke('app-info'), saveSettings:x=>ipcRenderer.invoke('save-settings',x),
  checkUpdates:()=>ipcRenderer.invoke('check-updates'),installUpdate:()=>ipcRenderer.invoke('install-update'),
  schoolAsk:x=>ipcRenderer.invoke('school-ask',x), researchAsk:x=>ipcRenderer.invoke('research-ask',x), planAsk:x=>ipcRenderer.invoke('plan-ask',x),
- onUpdateStatus:fn=>ipcRenderer.on('update-status',(_,x)=>fn(x))
+ diagnostics:()=>ipcRenderer.invoke('diagnostics'),clearLogs:()=>ipcRenderer.invoke('clear-logs'),onUpdateStatus:fn=>ipcRenderer.on('update-status',(_,x)=>fn(x))
 });
