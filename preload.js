4784d91c7aee69ecd945a351abd37c4bd821636c
flynndavies234-hub <flynndavies234@gmail.com>
2026-09-18 20:12:53 +0200

    Expose API connection test

--- preload.js
+++ preload.js
@@ -1,6 +1,6 @@
 const {contextBridge,ipcRenderer}=require('electron');
 contextBridge.exposeInMainWorld('desktopAPI',{
- getAppInfo:()=>ipcRenderer.invoke('app-info'), saveSettings:x=>ipcRenderer.invoke('save-settings',x),
+ getAppInfo:()=>ipcRenderer.invoke('app-info'), saveSettings:x=>ipcRenderer.invoke('save-settings',x),testApi:()=>ipcRenderer.invoke('test-api'),
  checkUpdates:()=>ipcRenderer.invoke('check-updates'),installUpdate:()=>ipcRenderer.invoke('install-update'),
  schoolAsk:x=>ipcRenderer.invoke('school-ask',x), researchAsk:x=>ipcRenderer.invoke('research-ask',x), planAsk:x=>ipcRenderer.invoke('plan-ask',x),
  diagnostics:()=>ipcRenderer.invoke('diagnostics'),clearLogs:()=>ipcRenderer.invoke('clear-logs'),onUpdateStatus:fn=>ipcRenderer.on('update-status',(_,x)=>fn(x))
